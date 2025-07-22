f3dd889d17fdeef5ec99e5f2bc3c1c252c92190a
kenny019 <ngkenny019@gmail.com>
2025-07-22 08:34:37 +0800

    fix: explorer url

--- app/_components/trade/details/tables/order-history/columns.tsx
+++ app/_components/trade/details/tables/order-history/columns.tsx
@@ -49,7 +49,7 @@ export const orderHistoryColumns: ColumnDef<MyTradeOrder, any>[] = [
 
       return (
         <Link
-          href={`https://explorer.twilight.rest/tx/${order.tx_hash}`}
+          href={`${process.env.NEXT_PUBLIC_EXPLORER_URL as string}/tx/${order.tx_hash}`}
           target="_blank"
           rel="noopener noreferrer"
           className="font-medium hover:underline"
